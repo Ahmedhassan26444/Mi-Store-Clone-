fac762a244ba292a84a4485c569b831f0ab8ee82
Ahmad <ahmedhassan.26444@gmail.com>
2026-03-30 16:06:41 +0500

    created navbar and slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import  Prenavbar from './components/prenavbar';
+import Prenavbar from './components/prenavbar';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Slider from './components/slider';
+import data from "./Data/Data.json"; 
+
 function App() {
   return (
-    <div>
+    <Router>
       <Prenavbar />
-    </div>
+      <Navbar />
+      <Slider start={data.banner.start} /> 
+    </Router>
   );
 }
 
