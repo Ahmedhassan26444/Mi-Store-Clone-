1864a065ea0ec8924d3483f9a5d681bfa349d2f5
Ahmad <ahmedhassan.26444@gmail.com>
2026-03-31 20:25:59 +0500

    minor changes in project

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import Navbar from './components/navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Slider from './components/slider';
 import data from "./Data/Data.json"; 
+import Offers from './components/Offers';
+import Heading from './components/Heading';
+import StarProduct from './components/StarProduct';
 
 function App() {
   return (
     <Router>
       <Prenavbar />
       <Navbar />
-      <Slider start={data.banner.start} /> 
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text="Star Products" />
+      <StarProduct starProduct={data.starProduct} />
     </Router>
   );
 }
