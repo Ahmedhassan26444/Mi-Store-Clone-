178fc18201c02fa0fe9380291f8037f280c0b988
Ahmad <ahmedhassan.26444@gmail.com>
2026-04-01 19:56:52 +0500

    created hot accessrioes heading and menu  and cover for menu of accessiores

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
 import './App.css';
 import Prenavbar from './components/prenavbar';
 import Navbar from './components/navbar';
-import { BrowserRouter as Router } from 'react-router-dom';
 import Slider from './components/slider';
 import data from "./Data/Data.json"; 
 import Offers from './components/Offers';
 import Heading from './components/Heading';
 import StarProduct from './components/StarProduct';
+import HotAccessoriesMenu from './components/HotAccessoriesMenu';
+import HotAccessories from './components/HotAccessories';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Prenavbar />
       <Navbar />
+
       <Slider start={data.banner.start} />
       <Offers offer={data.offer} />
+      
       <Heading text="Star Products" />
       <StarProduct starProduct={data.starProduct} />
+
+      <Heading text="Hot Accessories" />
+      <HotAccessoriesMenu />
+<Routes>
+  <Route 
+    path="/music"
+    element={
+      <HotAccessories 
+        music={data.hotAccessories.music}
+        musicCover={data.hotAccessoriesCover.music}
+      />
+    }
+  />
+  <Route 
+    path="/smart-devices"
+    element={
+      <HotAccessories 
+        smartDevice={data.hotAccessories.smartDevice}
+        smartDeviceCover={data.hotAccessoriesCover.smartDevice}
+      />
+    }
+  />
+  <Route 
+    path="/home"
+    element={
+      <HotAccessories 
+        home={data.hotAccessories.home}
+        homeCover={data.hotAccessoriesCover.home}
+      />
+    }
+  />
+  <Route 
+    path="/lifestyle"
+    element={
+      <HotAccessories 
+        lifeStyle={data.hotAccessories.lifeStyle}
+        lifeStyleCover={data.hotAccessoriesCover.lifeStyle}
+      />
+    }
+  />
+  <Route 
+    path="/mobile-accessories"
+    element={
+      <HotAccessories 
+        mobileAccessories={data.hotAccessories.mobileAccessories}
+        mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}
+      />
+    }
+  />
+</Routes>
     </Router>
   );
 }
